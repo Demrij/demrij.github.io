e6f4bbe778018382d692745bc5620bf3bc7dd504
demri <johnsondemri@gmail.com>
2024-11-17 16:44:46 -0700

    added a greeting and alert button section to html part of code

--- java.js
+++ java.js
@@ -2,3 +2,8 @@
 const footer = document.querySelector('footer p');
 const currentYear = new Date().getFullYear();
 footer.innerHTML = '&copy; 2024 Demri Johnson IDD Portfolio ${currentYear}. All rights reserved.';
+
+// display greeting based on time of the day //
+const greetingElement = document.createElement('p');
+const body = document.querySelector('body');
+body.insertBefore(greetingElement, body.firstChild);
