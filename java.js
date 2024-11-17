71b7adb533cc4a3012bb71bb385cc1041e2d4177
demri <johnsondemri@gmail.com>
2024-11-17 16:51:00 -0700

    started greeting element

--- java.js
+++ java.js
@@ -1,9 +1,7 @@
-// footer year using date method //
-const footer = document.querySelector('footer p');
-const currentYear = new Date().getFullYear();
-footer.innerHTML = '&copy; 2024 Demri Johnson IDD Portfolio ${currentYear}. All rights reserved.';
-
-// display greeting based on time of the day //
-const greetingElement = document.createElement('p');
-const body = document.querySelector('body');
-body.insertBefore(greetingElement, body.firstChild);
+// display greeting based in time of day //
+window.onload = function() {
+    const greetingElement = document.getElementById("greeting");
+    const hour = new Date().getHours();
+    let greetingMessage = "";
+    let greetingClass = "";
+}
